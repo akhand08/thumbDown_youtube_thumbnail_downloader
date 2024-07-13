e29379995156054a49ed9a18fdf3f8f0f7e222aa
akhand08 <adib08@outlook.com>
2024-07-13 12:49:23 +0600

    dependencies have been added. Then functionalities of choosing options to start or close the app has been implemented

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-import {intro, outro} from "@clack/prompts";
+import {intro, outro, select} from "@clack/prompts";
 import pc from 'picocolors'
 
 
 intro(pc.bgGreen(`Welcome to ${pc.bgRed("ThumbDown ")} - Youtube Video Thumbnail Download`));
+
+
+async function mainDownloader() {
+
+    while( true ) {
+
+        let options = await select({
+            "message": "Pick Your Option",
+            options: [
+                { value: "start", label: pc.green("Start the thumbDown App")},
+                {value: "close", label: pc.red("Close the thumbDown App")}
+            ]
+        })
+
+        if(options == "close") {
+            outro(pc.bgWhite(pc.black("thumbDown has been shut down successfully.")))
+            break;
+        }
+        else {
+            console.log("Working");
+        }
+    } 
+
+}
+
+mainDownloader();
